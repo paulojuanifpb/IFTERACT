58eb902b5ac65f45f1e4ea4fd0a659aab11d46e2
lucasdantas2014 <lucas.dantasifpb@gmail.com>
2017-11-08 16:25:25 -0300

    Criando Rotas de Login e consertando a de Cadastro

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,48 @@
-from flask import Flask, render_template, request
+from flask import Flask,  render_template, request
+from Tabelas.Usuario import *
 
 app = Flask(__name__)
 
-@app.route("/")
-def login():
+@app.route("/", methods=["GET"])
+def loginCadastro():
     print("Comeco")
-    return render_template("layout.html"), 200
+    return render_template("login.html"), 200
 
 
+@app.route("/logar", methods=["POST"])
+def logar():
+    user = request.json
+    usuarios = listarUsuarios()
+    perfilEncontrado = False
+    for usuario in usuarios:
+        if (usuario.email == user["email"]):
+            if(usuario.senha == user["senha"]):
+                perfilEncontrado = True
 
-@app.route("/cadastrar")
+    if (perfilEncontrado):
+        print(usuario)
+        return "ACHOU", 200
+
+    else:
+        return "ERROO", 200
+
+@app.route("/cadastrar", methods=["POST"])
 def cadastrar():
-    usuario = request.json()
-    print(usuario)
+    usuario = request.json
 
+    nome = usuario["nome"]
+    email = usuario["email"]
+    nascimento = usuario["nascimento"]
+    profissao = usuario["profissao"]
+    genero = usuario["genero"]
+    publico = usuario["publico"]
+    senha = usuario["senha"]
+
+    user  = Usuario(nome,email,nascimento,profissao, genero, publico, senha)
+
+    user.inserir(user)
     return "ok",200
 
 if __name__ == "__main__":
+
     app.run()
