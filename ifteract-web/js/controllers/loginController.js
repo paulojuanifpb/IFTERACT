9482c0aa42a797a3465e1ba95b0667a567fccebb
lucasdantas2014 <lucas.dantasifpb@gmail.com>
2017-11-30 15:40:37 -0200

    Alguns Ajustes

--- ifteract-web/js/controllers/loginController.js
+++ ifteract-web/js/controllers/loginController.js
@@ -9,15 +9,19 @@ app.controller('loginCtrl', function($scope, $state, usuarioService){
     $scope.cadastrar = function(){
 
         var user = $scope.usuario;
-
+        if(!user.publico){
+            user.publico = false;
+        }
         usuarioService.cadastrar(user);
         console.log("Deu certo");
+        console.log(user);
 
 
     };
 
     $scope.logar = function(perfil){
 
+        var perfil = $scope.perfil;
         console.log(perfil);
         
         var requisicao = usuarioService.logar(perfil);
